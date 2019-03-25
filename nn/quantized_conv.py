16db2ae7d655d79f298dd5814d9fbea465f2087d
hey-yahei <31240114+hey-yahei@users.noreply.github.com>
2019-03-25 16:02:15 +0800

    support quantized option for my Conv2D

--- nn/quantized_conv.py
+++ nn/quantized_conv.py
@@ -10,21 +10,18 @@ def _int2tuple(x):
     return (x, ) * 2 if isinstance(x, int) else x
 
 # Helper: im2col operation
-def _im2col_2D(F, x, kernel_size, strides, padding):
+def _im2col_2D(F, x, kernel_size, strides):
     # Get parameters
-    batch_size = x.shape[0]
+    batch_size, _, height, width = x.shape
     kh, kw = kernel_size
     sh, sw = strides
-    ph, pw = padding
-    # Pad
-    x_pad = x.pad(mode='constant', constant_value=0, pad_width=(0,0,0,0,ph,ph,pw,pw))
-    height, width = x_pad.shape[2], x_pad.shape[3]
+
     # Generate cols
     cols = []
     for h in range(0, height - kh + 1, sh):
         for w in range(0, width - kw + 1, sw):
             # Pick a window, reshape and append
-            win = x_pad[:, :, h:(h + kh), w:(w + kw)]
+            win = x[:, :, h:(h + kh), w:(w + kw)]
             col = win.reshape((batch_size, -1))     # batch_size x (kh * kw * in_channels)
             cols.append(col)
     # Calculate size of output feature map
@@ -33,10 +30,34 @@ def _im2col_2D(F, x, kernel_size, strides, padding):
     # Stack all cols into a matrix
     return F.stack(*cols, axis=1), out_h, out_w    # batch_size x (out_height * out_width) x (kh * kw * in_channels)
 
+def _quantize(F, x, min_range, max_range):
+    x = x.clip(min_range, max_range)
+    if max_range == -min_range:
+        scale = max_range / 127
+    else:
+        scale = (max_range - min_range) / 255
+    x = F.round(x / scale)
+    return F.cast(x, 'int32'), scale
+
+def quantize(F, x, out_type='int8'):
+    if out_type == 'int8':
+        max_range = x.abs().max().asscalar()
+        min_range = -max_range
+    elif out_type == 'uint8':
+        max_range = x.max().asscalar()
+        min_range = x.min().asscalar()
+    else:
+        raise ValueError("unknown out type: ", out_type)
+    return _quantize(F, x, min_range, max_range)
+
+def dequantize(F, x, scale):
+    x = F.cast(x, "float32")
+    return x * scale
+
 class Conv2D(nn.HybridBlock):
     def __init__(self, channels, kernel_size, strides, padding, in_channels, groups=1,
-                 activation=None, use_bias=True,
-                 input_dtype='float32', weight_dtype='float32', bias_dtype='float32',
+                 activation=None, use_bias=True, quantized=False,
+                 input_dtype='float32', weight_dtype='float32',
                  weight_initializer=None, bias_initializer='zero',
                  prefix=None, params=None):
         super(Conv2D, self).__init__(prefix, params)
@@ -50,12 +71,13 @@ class Conv2D(nn.HybridBlock):
             self._padding = _int2tuple(padding)
             self._kwargs = {
                 'kernel_size': self._kernel_size,
-                'strides': self._strides,
-                'padding': self._padding
+                'strides': self._strides
             }
+            self._quantized = quantized
             self._input_dtype = input_dtype
             self._weight_dtype = weight_dtype
-            self._bias_dtype = bias_dtype
+            self._input_range = None
+            self._weight_range = None
 
             self.weight = self.params.get('weight', shape=(channels, in_channels//groups, *self._kernel_size),
                                           init=weight_initializer, allow_deferred_init=True)
@@ -64,10 +86,25 @@ class Conv2D(nn.HybridBlock):
             self.act = nn.Activation(activation, prefix=activation+'_') if activation is not None else None
 
     def hybrid_forward(self, F, inputs, weight, bias=None):
-        # Cast
-        inputs = F.cast(inputs, self._input_dtype)
-        weight = F.cast(weight, self._weight_dtype)
-        bias = F.cast(bias, self._bias_dtype) if bias is not None else None
+        # Pad(note that F.pad is only support float32)
+        ph, pw = self._padding
+        inputs = inputs.pad(mode='constant', constant_value=0, pad_width=(0, 0, 0, 0, ph, ph, pw, pw))
+        # Quantize and cast into int32
+        if self._quantized:
+            if self._input_range is None:
+                inputs, in_scale = quantize(F, inputs, self._input_dtype)
+            else:
+                inputs, in_scale = _quantize(F, inputs, *self._input_range)
+            if self._weight_range is None:
+                weight, w_scale = quantize(F, weight, self._weight_dtype)
+            else:
+                weight, w_scale = _quantize(F, weight, *self._weight_range)
+            if bias is not None:
+                b_scale = in_scale * w_scale
+                b_max = in_scale * w_scale * (2 ** 31)
+                bias = F.clip(bias, -b_max, b_max)
+                bias = F.round(bias / b_scale)
+                bias = F.cast(bias, "int32")
         # Split inputs, weight and bias according to groups
         inputs = F.split(inputs, self._groups, axis=1) if self._groups > 1 else [inputs]
         weight = F.split(weight, self._groups, axis=0) if self._groups > 1 else [weight]
@@ -79,15 +116,27 @@ class Conv2D(nn.HybridBlock):
             x, out_h, out_w = _im2col_2D(F, x, **self._kwargs)  # batch_size x (out_height * out_width) x (kh * kw * in_channels)
             # Transport weight to matrix
             w = w.reshape((w.shape[0], -1))    # channels x (kh * kw * in_channels)
-            # Do convolution operation
-            x = F.dot(x, w, transpose_b=True)     # batch_size x (out_height * out_width) x channels
+            # Do convolution operation(note that F.dot only support float32)
+            if self._quantized:
+                x_fp = F.cast(x, 'float32')
+                w_fp = F.cast(w, 'float32')
+                x = F.dot(x_fp, w_fp, transpose_b=True)     # batch_size x (out_height * out_width) x channels
+                x = F.cast(x, 'int32')
+            else:
+                x = F.dot(x, w, transpose_b=True)  # batch_size x (out_height * out_width) x channels
             if b is not None:
                 x = x + b.reshape((1, 1, -1))
             # Rearrange output
             x = x.swapaxes(1, 2)    # batch_size x channels x (out_height * out_width)
             y.append(x.reshape((x.shape[0], x.shape[1], out_h, out_w)))
-        # Concat feature maps of every group
-        return F.concat(*y, dim=1)
+        # Concat feature maps of every group and apply activation
+        y = F.concat(*y, dim=1)
+        if self.act is not None:
+            y = self.act(y)
+        # Dequantize
+        if self._quantized:
+            y = dequantize(F, y, in_scale * w_scale)
+        return y
 
     def __repr__(self):
         s = '{name}({mapping}, kernel_size={kernel_size}, stride={strides}'
--- tests/test_quantized_conv.py
+++ tests/test_quantized_conv.py
@@ -28,10 +28,33 @@ def test_Conv2D(use_bias, groups):
 
     return (my_conv(x) - ref_conv(x)).abs().sum().asscalar()
 
+def test_quantized_Conv2D(use_bias, groups):
+    x = nd.uniform(shape=(2, 2, 5, 5))
+
+    my_conv = MyConv(10, 3, 1, 1, in_channels=2, groups=groups, use_bias=use_bias,
+                     input_dtype='uint8', weight_dtype='int8', quantized=True)
+    my_conv.initialize()
+
+    ref_conv = Conv2D(10, 3, 1, 1, in_channels=2, groups=groups, use_bias=use_bias,
+                      bias_initializer=init.Constant(my_conv.bias.data()) if use_bias else 'zero',
+                      weight_initializer=init.Constant(my_conv.weight.data()))
+    ref_conv.initialize()
+
+    my_res = my_conv(x)
+    ref_res = ref_conv(x)
+    return ((my_res - ref_res).abs() / ref_res.abs()).reshape(-1).sort()[-20:]
+
 if __name__ == "__main__":
     # test_im2col_2D()
-    print("Difference between my_conv and ref_conv -- ")
-    print("no bias, group=1: ", test_Conv2D(use_bias=False, groups=1))
-    print("bias, group=1   : ", test_Conv2D(use_bias=True, groups=1))
-    print("no bias, group=2: ", test_Conv2D(use_bias=False, groups=2))
-    print("bias, group=2   : ", test_Conv2D(use_bias=True, groups=2))
+
+    # print("Difference between my_conv and ref_conv -- ")
+    # print("no bias, group=1: ", test_Conv2D(use_bias=False, groups=1))
+    # print("bias, group=1   : ", test_Conv2D(use_bias=True, groups=1))
+    # print("no bias, group=2: ", test_Conv2D(use_bias=False, groups=2))
+    # print("bias, group=2   : ", test_Conv2D(use_bias=True, groups=2))
+
+    print("Difference between my_quantized_conv and ref_conv -- (error ratio[Top20]) ")
+    print(test_quantized_Conv2D(use_bias=False, groups=1))
+    print(test_quantized_Conv2D(use_bias=True, groups=1))
+    print(test_quantized_Conv2D(use_bias=False, groups=2))
+    print(test_quantized_Conv2D(use_bias=True, groups=2))
